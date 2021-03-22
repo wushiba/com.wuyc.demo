2a7f29748cc0b8b4e0243bd12944d7ed704c5bc5
x55555lg <15268848628@163.com>
2021-03-22 13:43:28 +0800

    add sa-token interceptor

--- com.yfshop.admin/com.yfshop.admin.web/src/main/java/com/yfshop/admin/config/WebMvcConfig.java
+++ com.yfshop.admin/com.yfshop.admin.web/src/main/java/com/yfshop/admin/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.yfshop.admin.config;
 
+import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
+import cn.dev33.satoken.interceptor.SaRouteInterceptor;
 import cn.hutool.core.date.DateUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -48,6 +50,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        new SaRouteInterceptor();
+        registry.addInterceptor(new SaAnnotationInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("");
     }
 
     /**
