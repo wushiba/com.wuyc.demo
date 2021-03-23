7a80c463e87b1cd58110e8f29fad7f96754dc28f
尤圣回 <626834850@qq.com>
2021-03-23 10:28:00 +0800

    工具类

---
+++ com.yfshop.common/src/main/java/com/yfshop/common/util/BeanUtil.java
@@ -0,0 +1,33 @@
+package com.yfshop.common.util;
+
+import cn.hutool.core.collection.CollectionUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeanUtil {
+
+    public static void copyProperties(Object source, Object target) throws BeansException {
+        BeanUtils.copyProperties(source, target);
+    }
+
+    public static <T, K> List<K> convertList(List<T> origList, Class<K> clazz) {
+        List<K> destList = new ArrayList<K>();
+        if (!CollectionUtil.isEmpty(origList)) {
+            try {
+                for (T original : origList) {
+                    K result = clazz.newInstance();
+                    copyProperties(result, original);
+                    destList.add(result);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        return destList;
+    }
+}
---
+++ com.yfshop.common/src/main/java/com/yfshop/common/util/DateUtil.java
@@ -0,0 +1,93 @@
+package com.yfshop.common.util;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 日期工具类
+ * Created by mu on 2019/1/29.
+ */
+public class DateUtil {
+
+    /**
+     * 从Date类型的时间中提取日期部分
+     */
+    public static Date getDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
+
+    /**
+     * 从Date类型的时间中提取时间部分
+     */
+    public static Date getTime(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.YEAR, 1970);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
+    }
+
+    //java.util.Date --> java.time.LocalDateTime
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        Instant instant = date.toInstant();
+        ZoneId zone = ZoneId.systemDefault();
+        return LocalDateTime.ofInstant(instant, zone);
+    }
+
+    //java.util.Date --> java.time.LocalDate
+    public static LocalDate dateToLocalDate(Date date) {
+        Instant instant = date.toInstant();
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zone);
+        return localDateTime.toLocalDate();
+    }
+
+    //java.util.Date --> java.time.LocalTime
+    public static LocalTime dateToLocalTime(Date date) {
+        Instant instant = date.toInstant();
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zone);
+        return localDateTime.toLocalTime();
+    }
+
+
+    // 04. java.time.LocalDateTime --> java.util.Date
+    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDateTime.atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+
+    // 05. java.time.LocalDate --> java.util.Date
+    public static Date localDateToDate(LocalDate localDate) {
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDate.atStartOfDay().atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+    // 06. java.time.LocalTime --> java.util.Date
+    public static Date localTimeToDate(LocalTime localTime) {
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDateTime.atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+    public static LocalDateTime localDateToLocalDateTime(LocalDate localDate) {
+        return dateToLocalDateTime(localDateToDate(localDate));
+    }
+
+//    public static void main(String[] args){
+//        System.out.println(localDateToLocalDateTime(LocalDate.now()));
+//    }
+}
