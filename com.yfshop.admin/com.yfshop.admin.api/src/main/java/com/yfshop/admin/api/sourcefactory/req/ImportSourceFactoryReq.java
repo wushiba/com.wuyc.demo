99d395660135c08eb26e0f96a5210748471a7c85
x55555lg <15268848628@163.com>
2021-03-26 10:54:12 +0800

    easypoi

--- com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/sourcefactory/req/ImportSourceFactoryReq.java
+++ com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/sourcefactory/req/ImportSourceFactoryReq.java
@@ -1,7 +1,10 @@
 package com.yfshop.admin.api.sourcefactory.req;
 
 import com.yfshop.admin.api.sourcefactory.excel.SourceFactoryExcel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -13,6 +16,9 @@ import java.util.List;
  * Created in 2021-03-25 19:20
  */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ImportSourceFactoryReq implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- com.yfshop.admin/com.yfshop.admin.web/src/main/java/com/yfshop/admin/controller/sourcefactory/AdminSourceFactoryManageController.java
+++ com.yfshop.admin/com.yfshop.admin.web/src/main/java/com/yfshop/admin/controller/sourcefactory/AdminSourceFactoryManageController.java
@@ -1,9 +1,31 @@
 package com.yfshop.admin.controller.sourcefactory;
 
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckRole;
+import com.yfshop.admin.api.sourcefactory.AdminSourceFactoryManageService;
+import com.yfshop.admin.api.sourcefactory.excel.SourceFactoryExcel;
+import com.yfshop.admin.api.sourcefactory.req.CreateSourceFactoryReq;
+import com.yfshop.admin.api.sourcefactory.req.ImportSourceFactoryReq;
+import com.yfshop.admin.api.sourcefactory.req.UpdateSourceFactoryReq;
+import com.yfshop.common.api.CommonResult;
 import com.yfshop.common.base.BaseController;
+import com.yfshop.common.exception.Asserts;
+import com.yfshop.common.util.ExcelUtils;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @author Xulg
@@ -13,4 +35,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("admin/sourceFactory")
 public class AdminSourceFactoryManageController implements BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(AdminSourceFactoryManageController.class);
+
+    @DubboReference(check = false)
+    private AdminSourceFactoryManageService adminSourceFactoryManageService;
+
+    @ApiOperation(value = "创建工厂", httpMethod = "GET")
+    @RequestMapping(value = "/createSourceFactory", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    @SaCheckLogin
+    @SaCheckRole(value = "sys")
+    public CommonResult<Void> createSourceFactory(@NotNull(message = "创建工厂信息不能为空") CreateSourceFactoryReq req) {
+        return CommonResult.success(adminSourceFactoryManageService.createSourceFactory(req));
+    }
+
+    @ApiOperation(value = "编辑工厂信息", httpMethod = "GET")
+    @RequestMapping(value = "/updateSourceFactory", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    @SaCheckLogin
+    @SaCheckRole(value = "sys")
+    public CommonResult<Void> updateSourceFactory(@NotNull(message = "编辑工厂信息不能为空") UpdateSourceFactoryReq req) {
+        return CommonResult.success(adminSourceFactoryManageService.updateSourceFactory(req));
+    }
+
+
+    @ApiOperation(value = "导入工厂数据Excel文件", httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "excelFile", value = "excel文件"),
+    })
+    @RequestMapping(value = "/importSourceFactory", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public CommonResult<Void> importSourceFactory(@NotNull(message = "导入文件不能为空") MultipartFile excelFile) {
+        List<SourceFactoryExcel> excels = ExcelUtils.importExcel(excelFile, 1, 1, SourceFactoryExcel.class);
+        Asserts.assertCollectionNotEmpty(excels, 500, "未能解析出Excel内容");
+        ImportSourceFactoryReq req = ImportSourceFactoryReq.builder().excels(excels).build();
+        return CommonResult.success(adminSourceFactoryManageService.importSourceFactory(req));
+    }
+
 }
