c6b261f3ab29c9970c07a77d004925cb7c99bbc9
wuyanchong <363648823@qq.com>
2021-04-01 17:07:59 +0800

    C端用户下单业务逻辑

--- com.yfshop.shop/com.yfshop.shop.api/src/main/java/com/yfshop/shop/service/mall/req/QueryItemDetailReq.java
+++ com.yfshop.shop/com.yfshop.shop.api/src/main/java/com/yfshop/shop/service/mall/req/QueryItemDetailReq.java
@@ -15,37 +15,4 @@ public class QueryItemDetailReq implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer itemId;
     private boolean querySku = true;
-
-    public static void main(String[] args) {
-        String a = "张三";
-        String b = "张三";
-        String c = new String("张三");
-        String d = new String("张三");
-        StringBuilder e = new StringBuilder("张三");
-        StringBuilder f = new StringBuilder("张三");
-        System.out.println(a == b);
-        System.out.println(a == c);
-        System.out.println(a == e.toString());
-        System.out.println(c == d);
-        System.out.println(c == f.toString());
-        System.out.println(e.toString() == f.toString());
-
-        Map<String, String> hashMap = new HashMap<>();
-        hashMap.put("key1", "value1");
-        hashMap.put("key2", "value3");
-        hashMap.put("key3", "value3");
-        Set<String> setList = hashMap.keySet();
-        Collection<String> valueList = hashMap.values();
-        for (String value : valueList) {
-            System.out.println(value);
-        }
-        for (String key : setList) {
-            System.out.println(key);
-        }
-        System.out.println(hashMap.containsValue("key3"));
-        System.out.println(hashMap.containsValue("value3"));
-        System.out.println(hashMap.isEmpty());
-        System.out.println(new HashedMap<>().isEmpty());
-    }
-
 }
