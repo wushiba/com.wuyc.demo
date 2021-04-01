561db3d35a0f3d0808afe1ae0d10109514569462
wuyanchong <363648823@qq.com>
2021-04-01 15:05:00 +0800

    前台订单相关接口, 还差下单接口没写

---
+++ com.yfshop.common/src/main/java/com/yfshop/common/enums/UserOrderStatusEnum.java
@@ -0,0 +1,45 @@
+package com.yfshop.common.enums;
+
+/**
+ * 用户订单状态枚举
+ * @author wuyc
+ * created 2021/4/1 10:03
+ **/
+public enum UserOrderStatusEnum {
+
+    WAIT_PAY("WAIT_PAY", "待付款"),
+    CANCEL("CANCEL", "已取消"),
+    WAIT_DELIVERY("WAIT_DELIVERY", "待发货"),
+    WAIT_RECEIVE("WAIT_RECEIVE", "待收货"),
+    SUCCESS("SUCCESS", "已完成");
+
+    private String code;
+
+    private String description;
+
+    UserOrderStatusEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static UserOrderStatusEnum getByCode(String code) {
+        if (code == null) {
+            return null;
+        }
+        for (UserOrderStatusEnum value : values()) {
+            if (value.getCode().equals(code)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
--- com.yfshop.shop/com.yfshop.shop.service/src/main/java/com/yfshop/shop/service/order/FrontUserOrderServiceImpl.java
+++ com.yfshop.shop/com.yfshop.shop.service/src/main/java/com/yfshop/shop/service/order/FrontUserOrderServiceImpl.java
@@ -1,19 +1,29 @@
 package com.yfshop.shop.service.order;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.yfshop.code.mapper.OrderAddressMapper;
 import com.yfshop.code.mapper.OrderDetailMapper;
 import com.yfshop.code.mapper.OrderMapper;
 import com.yfshop.code.mapper.UserCouponMapper;
 import com.yfshop.code.model.Order;
+import com.yfshop.code.model.OrderAddress;
 import com.yfshop.code.model.OrderDetail;
+import com.yfshop.code.model.UserCoupon;
+import com.yfshop.common.enums.UserCouponStatusEnum;
+import com.yfshop.common.enums.UserOrderStatusEnum;
 import com.yfshop.common.exception.ApiException;
 import com.yfshop.common.exception.Asserts;
-import com.yfshop.shop.service.order.result.OrderResult;
+import com.yfshop.common.util.BeanUtil;
+import com.yfshop.shop.service.order.result.YfUserOrderDetailResult;
+import com.yfshop.shop.service.order.result.YfUserOrderListResult;
 import com.yfshop.shop.service.order.service.FrontUserOrderService;
 import org.apache.dubbo.config.annotation.Service;
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Title:用户订单Service实现
@@ -38,15 +48,74 @@ public class FrontUserOrderServiceImpl implements FrontUserOrderService {
     private OrderAddressMapper orderAddressMapper;
 
 
+    /**
+     * 查询用户所有订单, 根据订单状态去组装. 因为单个用户不可能会有很多订单
+     * 待付款，已取消状态的订单，主订单信息是order里的数据， 子订单是orderDetail
+     * 待发货，待收货，已完成的订单，主订单是detail 里的数据， 子订单是orderDetail
+     * @param userId		用户id
+     * @param useStatus		订单状态
+     * @return
+     * @throws ApiException
+     */
     @Override
-    public List<OrderResult> findUserOrderList(Integer userId, String useStatus) throws ApiException {
+    public List<YfUserOrderListResult> findUserOrderList(Integer userId, String useStatus) throws ApiException {
+        Asserts.assertNonNull(userId, 500, "用户id不可以为空");
+        // 量大的话可以做10秒缓存
+        List<Order> orderList = orderMapper.selectList(Wrappers.lambdaQuery(Order.class)
+                .eq(Order::getUserId, userId).orderByDesc(Order::getId));
+        List<OrderDetail> detailList = orderDetailMapper.selectList(Wrappers.lambdaQuery(OrderDetail.class)
+                .eq(OrderDetail::getUserId, userId).orderByDesc(OrderDetail::getId));
 
-        return null;
+        List<YfUserOrderListResult> dataList = setUserOrderListResult(orderList, detailList);
+        if (StringUtils.isBlank(useStatus)) {
+            return dataList;
+        }
+        Map<String, List<YfUserOrderListResult>> dataListMap = dataList.stream().collect(Collectors.groupingBy(YfUserOrderListResult::getOrderStatus));
+        return dataListMap.get(useStatus);
     }
 
+    /**
+     * 订单id为空说明查的是子订单详情
+     * 订单id不为空代表查的是整个订单的详情，一般用于未支付，订单取消的订单
+     * @param userId		用户id
+     * @param orderId		订单id
+     * @param orderDetailId
+     * @return
+     * @throws ApiException
+     */
     @Override
-    public List<OrderResult> getUserOrderDetail(Integer userId, Integer orderId) throws ApiException {
-        return null;
+    public YfUserOrderDetailResult getUserOrderDetail(Integer userId, Integer orderId, Integer orderDetailId) throws ApiException {
+        // todo 量大的话可以做1分钟秒缓存
+        Asserts.assertFalse(orderId == null && orderDetailId == null , 500, "订单标识不可以为空");
+        YfUserOrderDetailResult userOrderDetailResult;
+
+        Order order = orderMapper.selectOne(Wrappers.lambdaQuery(Order.class)
+                .eq(Order::getUserId, userId)
+                .eq(Order::getId, orderId));
+        List<OrderDetail> detailList = orderDetailMapper.selectList(Wrappers.lambdaQuery(OrderDetail.class)
+                .eq(OrderDetail::getUserId, userId).eq(OrderDetail::getOrderId, orderId).orderByDesc(OrderDetail::getId));
+
+        if (orderDetailId == null) {
+            List<YfUserOrderDetailResult.YfUserOrderItem> itemList = BeanUtil.convertList(detailList, YfUserOrderDetailResult.YfUserOrderItem.class);
+            userOrderDetailResult = BeanUtil.convert(order, YfUserOrderDetailResult.class);
+            userOrderDetailResult.setOrderStatus(detailList.get(0).getOrderStatus());
+            userOrderDetailResult.setOrderId(order.getId());
+            userOrderDetailResult.setItemList(itemList);
+        } else {
+            List<OrderDetail> itemList = detailList.stream().filter(data -> data.getId().equals(orderDetailId)).collect(Collectors.toList());
+            List<YfUserOrderDetailResult.YfUserOrderItem> resultItemList = BeanUtil.convertList(itemList, YfUserOrderDetailResult.YfUserOrderItem.class);
+            userOrderDetailResult = BeanUtil.convert(itemList.get(0), YfUserOrderDetailResult.class);
+            userOrderDetailResult.setOrderId(order.getId());
+            userOrderDetailResult.setOrderDetailId(itemList.get(0).getId());
+            userOrderDetailResult.setOrderStatus(detailList.get(0).getOrderStatus());
+            userOrderDetailResult.setItemList(resultItemList);
+        }
+
+        // 设置收货地址
+        OrderAddress orderAddress = orderAddressMapper.selectOne(Wrappers.lambdaQuery(OrderAddress.class).eq(OrderAddress::getOrderId, orderId));
+        YfUserOrderDetailResult.YfUserOrderAddress addressInfo = BeanUtil.convert(orderAddress, YfUserOrderDetailResult.YfUserOrderAddress.class);
+        userOrderDetailResult.setAddressInfo(addressInfo);
+        return userOrderDetailResult;
     }
 
     @Override
@@ -55,9 +124,17 @@ public class FrontUserOrderServiceImpl implements FrontUserOrderService {
                 .eq(Order::getUserId, userId)
                 .eq(Order::getId, orderId));
         Asserts.assertNonNull(order, 500, "主订单信息不存在");
-        Asserts.assertNotEquals("Y", order.getIsPay(), 500, "订单已支付,不能取消。");
+        Asserts.assertNotEquals("Y", order.getIsPay(), 500, "订单已支付,不能取消.");
 
-//
+        // 退还优惠券.优惠券改成未使用
+        List<OrderDetail> detailList = orderDetailMapper.selectList(Wrappers.lambdaQuery(OrderDetail.class).eq(OrderDetail::getOrderId, orderId));
+        detailList.forEach(orderDetail -> {
+            if (orderDetail.getUserCouponId() != null) {
+                UserCoupon userCoupon = new UserCoupon();
+                userCoupon.setUseStatus(UserCouponStatusEnum.NO_USE.getCode());
+                userCouponMapper.updateById(userCoupon);
+            }
+        });
     }
 
     @Override
@@ -74,6 +151,64 @@ public class FrontUserOrderServiceImpl implements FrontUserOrderService {
 
         orderDetail.setOrderStatus("SUCCESS");
         orderDetailMapper.updateById(orderDetail);
+
+        if (orderDetail.getUserCouponId() != null) {
+            // 优惠券改成已使用
+            UserCoupon userCoupon = new UserCoupon();
+            userCoupon.setUseStatus(UserCouponStatusEnum.HAS_USE.getCode());
+            userCouponMapper.updateById(userCoupon);
+        }
     }
+
+
+    private List<YfUserOrderListResult> setUserOrderListResult(List<Order> orderList, List<OrderDetail> childList) {
+        List<YfUserOrderListResult> resultList = new ArrayList<>();
+        for (Order order : orderList) {
+            YfUserOrderListResult orderResult;
+            List<OrderDetail> detailList = childList.stream().filter(data -> data.getOrderId().equals(order.getId())).collect(Collectors.toList());
+            if ("N".equalsIgnoreCase(order.getIsPay()) && "N".equalsIgnoreCase(order.getIsCancel())) {
+                // 未付款订单状态组装
+                orderResult = new YfUserOrderListResult();
+                orderResult.setOrderStatus(UserOrderStatusEnum.WAIT_PAY.getCode());
+                orderResult.setOrderId(order.getId());
+                orderResult.setOrderDetailId(null);
+                orderResult.setPayPrice(order.getPayPrice());
+                orderResult.setOrderPrice(order.getOrderPrice());
+                List<YfUserOrderListResult.YfUserOrderItem> resultChildList = BeanUtil.convertList(detailList, YfUserOrderListResult.YfUserOrderItem.class);
+                orderResult.setItemList(resultChildList);
+                resultList.add(orderResult);
+            } else if ("N".equalsIgnoreCase(order.getIsPay()) && "Y".equalsIgnoreCase(order.getIsCancel())) {
+                // 已取消订单状态组装
+                orderResult = new YfUserOrderListResult();
+                orderResult.setOrderStatus(UserOrderStatusEnum.CANCEL.getCode());
+                orderResult.setOrderId(order.getId());
+                orderResult.setOrderDetailId(null);
+                orderResult.setPayPrice(order.getPayPrice());
+                orderResult.setOrderPrice(order.getOrderPrice());
+                List<YfUserOrderListResult.YfUserOrderItem> resultChildList = BeanUtil.convertList(detailList, YfUserOrderListResult.YfUserOrderItem.class);
+                orderResult.setItemList(resultChildList);
+                resultList.add(orderResult);
+            } else {
+                // 待发货，待收货，已完成订单状态组装数据
+                for (OrderDetail orderDetail : detailList) {
+                    orderResult = new YfUserOrderListResult();
+                    orderResult.setOrderStatus(orderDetail.getOrderStatus());
+                    orderResult.setOrderId(orderDetail.getOrderId());
+                    orderResult.setOrderDetailId(orderDetail.getId());
+                    orderResult.setPayPrice(orderDetail.getPayPrice());
+                    orderResult.setOrderPrice(orderDetail.getOrderPrice());
+                    orderResult.setExpressNo(orderDetail.getExpressNo());
+                    List<YfUserOrderListResult.YfUserOrderItem> resultChildList = new ArrayList<>();
+                    YfUserOrderListResult.YfUserOrderItem userOrderItem = BeanUtil.convert(orderDetail, YfUserOrderListResult.YfUserOrderItem.class);
+                    resultChildList.add(userOrderItem);
+                    orderResult.setItemList(resultChildList);
+                    resultList.add(orderResult);
+                }
+            }
+        }
+        return resultList;
+    }
+
+
 }
 
