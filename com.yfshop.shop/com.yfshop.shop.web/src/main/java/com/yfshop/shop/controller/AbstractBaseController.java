6662ffbcf8cd1830b3329e38d191716c9796d6ac
尤圣回 <626834850@qq.com>
2021-04-07 18:44:38 +0800

    提交代码

--- com.yfshop.shop/com.yfshop.shop.web/src/main/java/com/yfshop/shop/controller/AbstractBaseController.java
+++ com.yfshop.shop/com.yfshop.shop.web/src/main/java/com/yfshop/shop/controller/AbstractBaseController.java
@@ -1,21 +1,20 @@
 package com.yfshop.shop.controller;
 
-import cn.dev33.satoken.dao.SaTokenDaoRedisJackson;
+import com.yfshop.auth.api.service.AuthService;
 import com.yfshop.common.base.BaseController;
-import com.yfshop.shop.config.WxStpLogic;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.dubbo.config.annotation.DubboReference;
 
 public abstract class AbstractBaseController implements BaseController {
 
-    @Autowired
-    SaTokenDaoRedisJackson saTokenDaoRedis;
+    @DubboReference
+    AuthService authService;
 
     @Override
     public String getCurrentOpenId() {
         String openId = getCookieValue("yfopen");
         if (StringUtils.isNotBlank(openId)) {
-            String id =saTokenDaoRedis.get(String.format("yfopen:login:token:%s", openId));
+            String id =authService.get(String.format("yfopen:login:token:%s", openId));
             return id;
         }
         return null;
