c37c8e89fcda0624711fe11da655a46ee736eb1f
尤圣回 <626834850@qq.com>
2021-04-08 14:29:48 +0800

    提交代码

---
+++ com.yfshop.wx/com.yfshop.wx.web/src/main/java/com/yfshop/wx/AdminWxApplication.java
@@ -0,0 +1,25 @@
+package com.yfshop.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@ServletComponentScan
+@SpringBootApplication
+public class AdminWxApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AdminWxApplication.class, args);
+        String property = applicationContext.getEnvironment().getProperty("spring.profiles.active");
+        System.out.println("==============启动了===com.yfshop.admin.wx====================环境===" + property);
+    }
+
+}
