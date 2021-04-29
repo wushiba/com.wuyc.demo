7f37dc4fbcfb5bfb57fc06729640b19422e1fdcb
尤圣回 <626834850@qq.com>
2021-04-29 13:24:31 +0800

    11

--- com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/website/WebsiteCodeTaskService.java
+++ com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/website/WebsiteCodeTaskService.java
@@ -1,8 +1,7 @@
 package com.yfshop.admin.api.website;
 
-import io.swagger.models.auth.In;
-
 public interface WebsiteCodeTaskService {
+
    void buildWebSiteCode(Integer id);
 
    void doWorkWebsiteCodeFile(String outTradeNo);
