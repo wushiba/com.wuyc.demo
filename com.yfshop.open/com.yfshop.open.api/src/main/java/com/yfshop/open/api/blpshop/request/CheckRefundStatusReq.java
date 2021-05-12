96d1ecbed17ea549336a7d349facdd96eb3e72f7
尤圣回 <626834850@qq.com>
2021-05-12 10:19:44 +0800

    1

--- com.yfshop.open/com.yfshop.open.api/src/main/java/com/yfshop/open/api/blpshop/request/CheckRefundStatusReq.java
+++ com.yfshop.open/com.yfshop.open.api/src/main/java/com/yfshop/open/api/blpshop/request/CheckRefundStatusReq.java
@@ -1,7 +1,10 @@
 package com.yfshop.open.api.blpshop.request;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class CheckRefundStatusReq implements Serializable {
     private String orderId;
 }
--- com.yfshop.open/com.yfshop.open.web/src/main/java/com/yfshop/open/controller/BlpShopController.java
+++ com.yfshop.open/com.yfshop.open.web/src/main/java/com/yfshop/open/controller/BlpShopController.java
@@ -20,59 +20,83 @@ public class BlpShopController implements BaseController {
     @DubboReference
     private OrderService orderService;
 
-    /**
-     * 下载订单
-     *
-     * @param orderReq
-     * @return
-     */
-    @RequestMapping(value = "/getOrder", method = {RequestMethod.POST})
-    public OrderResult getOrder(OrderReq orderReq) {
-        checkWhiteIp();
-        return orderService.getOrder(orderReq);
-    }
 
     /**
-     * 发货信息
-     *
-     * @param sendReq
      * @return
      */
-    @RequestMapping(value = "/send", method = {RequestMethod.POST})
-    public SendResult send(SendReq sendReq) {
+    @RequestMapping(value = "/api", method = {RequestMethod.POST})
+    public Object api(ApiReq apiReq) {
         checkWhiteIp();
-        return orderService.send(sendReq);
+        Asserts.assertTrue(apiReq.checkSign(), 500, "签名校验失败！");
+        switch (apiReq.getMethod()) {
+            case "Differ.JH.Business.GetOrder":
+                return orderService.getOrder(apiReq.getReq(OrderReq.class));
+            case "Differ.JH.Business.CheckRefundStatus":
+                return orderService.checkRefundStatus(apiReq.getReq(CheckRefundStatusReq.class));
+            case "Differ.JH.Business.Send":
+                return orderService.send(apiReq.getReq(SendReq.class));
+            case "Differ.JH.Business.DownloadProduct":
+                return orderService.downloadProduct(apiReq.getReq(DownloadProductReq.class));
+            case "Differ.JH.Business.GetRefund":
+                return orderService.getRefund(apiReq.getReq(RefundReq.class));
+        }
+        Asserts.assertTrue(false, 500, "没找到对应的接口方法！");
+        return null;
     }
 
-    /**
-     * 校验退款状态
-     *
-     * @param checkRefundStatusReq
-     * @return
-     */
-    @RequestMapping(value = "/checkRefundStatus", method = {RequestMethod.POST})
-    public CheckRefundStatusResult checkRefundStatus(CheckRefundStatusReq checkRefundStatusReq) {
-        checkWhiteIp();
-        return orderService.checkRefundStatus(checkRefundStatusReq);
-    }
-
-    @RequestMapping(value = "/downloadProduct", method = {RequestMethod.POST})
-    public DownloadProductResult downloadProduct(DownloadProductReq downloadProductReq) {
-        checkWhiteIp();
-        return orderService.downloadProduct(downloadProductReq);
-    }
-
-    @RequestMapping(value = "/syncStock", method = {RequestMethod.POST})
-    public SyncStockResult syncStock(SyncStockReq syncStockReq) {
-        checkWhiteIp();
-        return orderService.syncStock(syncStockReq);
-    }
-
-    @RequestMapping(value = "/getRefund", method = {RequestMethod.POST})
-    public RefundResult getRefund(RefundReq refundReq) {
-        checkWhiteIp();
-        return orderService.getRefund(refundReq);
-    }
+//    /**
+//     * 下载订单
+//     *
+//     * @param orderReq
+//     * @return
+//     */
+//    @RequestMapping(value = "/getOrder", method = {RequestMethod.POST})
+//    public OrderResult getOrder(OrderReq orderReq) {
+//        checkWhiteIp();
+//        return orderService.getOrder(orderReq);
+//    }
+//
+//    /**
+//     * 发货信息
+//     *
+//     * @param sendReq
+//     * @return
+//     */
+//    @RequestMapping(value = "/send", method = {RequestMethod.POST})
+//    public SendResult send(SendReq sendReq) {
+//        checkWhiteIp();
+//        return orderService.send(sendReq);
+//    }
+//
+//    /**
+//     * 校验退款状态
+//     *
+//     * @param checkRefundStatusReq
+//     * @return
+//     */
+//    @RequestMapping(value = "/checkRefundStatus", method = {RequestMethod.POST})
+//    public CheckRefundStatusResult checkRefundStatus(CheckRefundStatusReq checkRefundStatusReq) {
+//        checkWhiteIp();
+//        return orderService.checkRefundStatus(checkRefundStatusReq);
+//    }
+//
+//    @RequestMapping(value = "/downloadProduct", method = {RequestMethod.POST})
+//    public DownloadProductResult downloadProduct(DownloadProductReq downloadProductReq) {
+//        checkWhiteIp();
+//        return orderService.downloadProduct(downloadProductReq);
+//    }
+//
+//    @RequestMapping(value = "/syncStock", method = {RequestMethod.POST})
+//    public SyncStockResult syncStock(SyncStockReq syncStockReq) {
+//        checkWhiteIp();
+//        return orderService.syncStock(syncStockReq);
+//    }
+//
+//    @RequestMapping(value = "/getRefund", method = {RequestMethod.POST})
+//    public RefundResult getRefund(RefundReq refundReq) {
+//        checkWhiteIp();
+//        return orderService.getRefund(refundReq);
+//    }
 
 
     public void checkWhiteIp() {
