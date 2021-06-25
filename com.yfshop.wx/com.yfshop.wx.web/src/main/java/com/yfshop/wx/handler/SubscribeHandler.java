bf7d87e2dc4bf830779301942b8d82b55b31586f
尤圣回 <626834850@qq.com>
2021-06-26 02:14:47 +0800

    1

--- com.yfshop.wx/com.yfshop.wx.web/src/main/java/com/yfshop/wx/handler/SubscribeHandler.java
+++ com.yfshop.wx/com.yfshop.wx.web/src/main/java/com/yfshop/wx/handler/SubscribeHandler.java
@@ -68,7 +68,12 @@ public class SubscribeHandler extends AbstractHandler {
         }
 
         try {
-            String msg = userService.getSubscribeMsg();
+           // String msg = userService.getSubscribeMsg();
+            String msg ="hi，我们是光明乳业、椰岛集团官方合作商\n" +
+                    "\n" +
+                    "雨帆健康家--给家人以温暖，给生活以健康\n" +
+                    " \n" +
+                    "限时大促抢购：https://m.yufanlook.com/#/ActPage";
             if (StringUtils.isNotBlank(msg)) {
                 return new TextBuilder().build(msg, wxMessage, weixinService);
             }
