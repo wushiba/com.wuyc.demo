6322eb9df304aef3100ae97e72962fa007f44840
尤圣回 <626834850@qq.com>
2021-07-06 15:30:23 +0800

    1

--- com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/coupon/result/CouponRulesResult.java
+++ com.yfshop.admin/com.yfshop.admin.api/src/main/java/com/yfshop/admin/api/coupon/result/CouponRulesResult.java
@@ -1,35 +1,17 @@
 package com.yfshop.admin.api.coupon.result;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.yfshop.common.validate.annotation.CandidateValue;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class CouponRulesResult implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime createTime;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime updateTime;
-
+    
     private Integer couponId;
 
 
