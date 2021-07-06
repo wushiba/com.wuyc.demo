bd85899b06aec0dec1c98d7db51228a42463c761
尤圣回 <626834850@qq.com>
2021-07-06 18:46:47 +0800

    1

--- com.yfshop.shop/com.yfshop.shop.service/src/main/java/com/yfshop/shop/service/coupon/FrontUserCouponServiceImpl.java
+++ com.yfshop.shop/com.yfshop.shop.service/src/main/java/com/yfshop/shop/service/coupon/FrontUserCouponServiceImpl.java
@@ -180,8 +180,9 @@ public class FrontUserCouponServiceImpl implements FrontUserCouponService {
             ItemSku sku = skuIndexMap.get(u.getSkuId());
             if (sku != null && sku.getCategoryId() == 3) {
                 if ("TC".equals(sku.getSkuType())) {
-                    pay = pay.add(sku.getSkuSalePrice().multiply(new BigDecimal(u.getNum())));
                     isTc = true;
+                } else {
+                    pay = pay.add(sku.getSkuSalePrice().multiply(new BigDecimal(u.getNum())));
                 }
             }
             if (!isTc) {
