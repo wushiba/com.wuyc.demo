b5b3881dcdcde3102b89b746d839ae53618962d8
尤圣回 <626834850@qq.com>
2021-07-20 12:16:10 +0800

    提交推送参数代码

---
+++ com.yfshop.code/src/main/java/com/yfshop/code/mapper/WxPushTaskExtendMapper.java
@@ -0,0 +1,16 @@
+package com.yfshop.code.mapper;
+
+import com.yfshop.code.model.WxPushTaskExtend;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yoush
+ * @since 2021-07-20
+ */
+public interface WxPushTaskExtendMapper extends BaseMapper<WxPushTaskExtend> {
+
+}
