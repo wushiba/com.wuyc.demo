29a564b607a37d18ed1843e91be323cd45811fb9
尤圣回 <626834850@qq.com>
2021-09-18 09:53:27 +0800

    订单排序问题

--- com.yfshop.admin/com.yfshop.admin.service/src/main/java/com/yfshop/admin/service/spread/SpreadServiceImpl.java
+++ com.yfshop.admin/com.yfshop.admin.service/src/main/java/com/yfshop/admin/service/spread/SpreadServiceImpl.java
@@ -144,6 +144,7 @@ public class SpreadServiceImpl implements SpreadService {
                                 })
                         .gt(spreadOrderReq.getStartTime() != null, SpreadOrder::getCreateTime, spreadOrderReq.getStartTime())
                         .lt(spreadOrderReq.getEndTime() != null, SpreadOrder::getCreateTime, spreadOrderReq.getEndTime())
+                        .orderByDesc(SpreadOrder::getId)
         );
         return BeanUtil.iPageConvert(iPage, SpreadOrderResult.class);
     }
@@ -185,6 +186,7 @@ public class SpreadServiceImpl implements SpreadService {
                 .eq(spreadBillReq.getType() != null, SpreadBill::getType, spreadBillReq.getType())
                 .ge(spreadBillReq.getStartTime() != null, SpreadBill::getUpdateTime, spreadBillReq.getStartTime())
                 .lt(spreadBillReq.getEndTime() != null, SpreadBill::getUpdateTime, spreadBillReq.getEndTime())
+                .orderByDesc(SpreadBill::getId)
         );
         return BeanUtil.iPageConvert(iPage, SpreadBillResult.class);
     }
